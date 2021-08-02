7de2f8bd2becbf2c811b23720fd16ea99b26833c
Kanai Masumi <m-kanai@bbsakura.net>
2021-08-02 16:09:32 +0900

    change base to use replaced interface

--- main.go
+++ main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/masu-mi/test_gomod_replace_dep_a"
+	test_gomod_replace_dep_a "github.com/masu-mi/test_gomod_replace_dep_a"
+	"github.com/masu-mi/test_gomod_replace_dep_a/interfaces"
 )
 
 func main() {
 	fmt.Println("IN main")
 	fmt.Printf("%d\n", test_gomod_replace_dep_a.Add(1, 2))
+	fmt.Printf("%d\n", test_gomod_replace_dep_a.Add((&sample{}).Write("100000"), 2))
+}
+
+type sample struct{}
+var _ interfaces.Writer = &sample{}
+
+func (s *sample) Write(payload string) (num test_gomod_replace_dep_a.OriginalInt) {
+	return test_gomod_replace_dep_a.OriginalInt(len(payload))
 }
